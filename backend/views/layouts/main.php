0d998e1bd9f61126baeb091260746177ec9d7271
Andrey Khan <andrkh@gmail.com>
2025-02-21 10:56:20 +0500

    Fix Footer position

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -14,7 +14,7 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100" data-theme="modern" data-layout="vertical">
+<html lang="<?= Yii::$app->language ?>" data-theme="modern" data-layout="vertical">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -23,7 +23,7 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?> | <?= Yii::$app->name ?></title>
     <?php $this->head() ?>
 </head>
-<body class="d-flex flex-column h-100">
+<body class="d-flex flex-column">
 <?php $this->beginBody() ?>
 <div id="layout-wrapper">
     <?= $this->render('topbar') ?>
