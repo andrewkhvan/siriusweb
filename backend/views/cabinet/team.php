a6d7978a10a6f7a5e2f814d8ce376cf505e4f33f
Andrey Khan <andrkh@gmail.com>
2025-02-26 20:56:26 +0500

    Move structure to team_subgroup view, add fields

--- backend/views/cabinet/team.php
+++ backend/views/cabinet/team.php
@@ -90,29 +90,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>
                 </div>
                 <div class="list-group nested-list">
-                    <?php foreach ($data->rows as $member): ?>
-                    <div class="list-group-item pe-1">
-                        <div class="d-flex align-items-center">
-                            <div class="flex-shrink-0 me-3">
-                                <i class="mdi mdi-account-circle mdi-36px text-secondary"></i>
-                            </div>
-                            <div class="flex-grow-1">
-                                <h5 class="fs-14"><?= $member->Name ?></h5>
-                                <p class="text-muted mb-0">Rank-<?= $member->Rank ?></p>
-                            </div>
-                            <?php if ($member->HasChildren): ?>
-                            <div class="flex-shrink-0">
-                                <a href="#" data-bs-toggle="collapse" data-bs-target="#children-<?= $member->PartnerId?>" data-id="<?= $member->PartnerId ?>" class="toggle-subgroup">
-                                    <i class="mdi mdi-plus-box-outline mdi-24px"></i>
-                                </a>
-                            </div>
-                            <?php endif; ?>
-                        </div>
-                        <?php if ($member->HasChildren): ?>
-                        <div class="collapse" id="children-<?= $member->PartnerId?>"></div>
-                        <?php endif; ?>
-                    </div>
-                    <?php endforeach; ?>
+                    <?= $this->render('team_subgroup', ['data' => $data]) ?>
                 </div>
             </div><!-- end card body -->
         </div><!-- end card -->
--- backend/views/cabinet/team_subgroup.php
+++ backend/views/cabinet/team_subgroup.php
@@ -1,23 +1,25 @@
 <?php foreach ($data->rows as $member): ?>
 <div class="list-group-item pe-1">
-    <div class="d-flex align-items-center">
+    <div class="d-flex">
         <div class="flex-shrink-0 me-3">
             <i class="mdi mdi-account-circle mdi-36px text-secondary"></i>
         </div>
         <div class="flex-grow-1">
-            <h5 class="fs-14"><?= $member->Name ?></h5>
-            <p class="text-muted mb-0">Rank-<?= $member->Rank ?></p>
+            <h5 class="fs-14"><?= $member->Name ?> <span class="text-muted ps-2">Rank-<?= $member->Rank ?></span></h5>
+            <div class="text-muted mb-2"><?= date('Y-m-d H:i:s', $member->RegistrationDate) ?></div>
+            <p>Invested <?= number_format($member->InvestmentValue, 2, '.', ' ') ?></p>
         </div>
+        <div class="flex-shrink-0 ms-2 text-end">
+            <div class="mb-2 text-muted">Level <?= $member->Level ?></div>
         <?php if ($member->HasChildren): ?>
-        <div class="flex-shrink-0">
             <a href="#" data-bs-toggle="collapse" data-bs-target="#children-<?= $member->PartnerId?>" data-id="<?= $member->PartnerId ?>" class="toggle-subgroup">
                 <i class="mdi mdi-plus-box-outline mdi-24px"></i>
             </a>
-        </div>
         <?php endif; ?>
+        </div>
     </div>
-    <?php if ($member->HasChildren): ?>
+<?php if ($member->HasChildren): ?>
     <div class="collapse" id="children-<?= $member->PartnerId?>"></div>
-    <?php endif; ?>
+<?php endif; ?>
 </div>
 <?php endforeach; ?>
