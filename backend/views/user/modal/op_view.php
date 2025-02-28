eff3725dfc2e74035ac0f6aefb750995c49f7f20
Andrey Khan <andrkh@gmail.com>
2025-02-28 17:28:10 +0500

    update popup view

--- backend/views/user/modal/op_view.php
+++ backend/views/user/modal/op_view.php
@@ -3,27 +3,27 @@
     <div class="col-8"><p><?= date('m/d/Y H:i:s', $data->Date) ?></p></div>
 </div>
 <div class="row">
-    <div class="col-4"><p class="fw-bold text-end">DocNo</p></div>
+    <div class="col-4"><p class="fw-bold text-end">Number</p></div>
     <div class="col-8"><p><?= $data->DocNo ?></p></div>
-</div>
-<div class="row">
-    <div class="col-4"><p class="fw-bold text-end">Operation Status</p></div>
-    <div class="col-8"><p><?= $data->OperationStatus ?></p></div>
 </div>
 <div class="row">
     <div class="col-4"><p class="fw-bold text-end">Operation</p></div>
     <div class="col-8"><p><?= $data->Operation ?></p></div>
 </div>
 <div class="row">
-    <div class="col-4"><p class="fw-bold text-end">DocSum</p></div>
+    <div class="col-4"><p class="fw-bold text-end">Operation Status</p></div>
+    <div class="col-8"><p><?= $data->OperationStatus ?></p></div>
+</div>
+<div class="row">
+    <div class="col-4"><p class="fw-bold text-end">Sum</p></div>
     <div class="col-8"><p><?= $data->DocSum ?></p></div>
 </div>
 <div class="row">
-    <div class="col-4"><p class="fw-bold text-end">PartnerName</p></div>
+    <div class="col-4"><p class="fw-bold text-end">Name</p></div>
     <div class="col-8"><p><?= $data->PartnerName ?></p></div>
 </div>
 <div class="row">
-    <div class="col-4"><p class="fw-bold text-end">Partner Email</p></div>
+    <div class="col-4"><p class="fw-bold text-end">Email</p></div>
     <div class="col-8"><p><?= $data->PartnerEmail ?></p></div>
 </div>
 <div class="row">
@@ -43,16 +43,12 @@
     <div class="col-8"><p><i class="mdi mdi-18px mdi-checkbox-<?= $data->Virtual ? 'marked':'blank' ?>-outline"></i></p></div>
 </div>
 <div class="row">
-    <div class="col-4"><p class="fw-bold text-end">WAddress</p></div>
+    <div class="col-4"><p class="fw-bold text-end">Wallet</p></div>
     <div class="col-8"><p><?= $data->WAddress ?></p></div>
 </div>
 
-<div class="pt-2 text-right">
-<?php if ($data->HasApproveButton): ?>
-    <button type="button" class="btn btn-success ">Approve</button>
-<?php endif; ?>
-
-<?php if ($data->HasCancelButton): ?>
-    <button type="button" class="btn btn-soft-dark ">Cancel</button>
-<?php endif; ?>
+<div class="pt-2 text-end">
+<button class="btn btn-soft-dark" data-bs-dismiss="modal">Close</button>
+    <button type="button" class="btn btn-success<?= $data->HasApproveButton ? '': ' disabled' ?>">Approve</button>
+    <button type="button" class="btn btn-danger<?= $data->HasCancelButton ? '': ' disabled' ?>">Decline</button>
 </div>
