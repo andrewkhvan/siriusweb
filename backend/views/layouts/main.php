4bbc66248eca6063c585bb1e310193cf093e38b9
Andrey Khvan <83340755+andrewkhvan@users.noreply.github.com>
2025-09-10 22:52:31 +0500

    Абонентка

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -70,6 +70,40 @@ AppAsset::register($this);
 
 </div>
 
+<?php if (!Yii::$app->user->isGuest && !Yii::$app->user->identity->paid): ?>
+    <?php
+    $this->registerCss('.modal-backdrop.show{backdrop-filter:blur(5px);}');
+    $js = <<<JS
+    var paidModal = new bootstrap.Modal(document.getElementById('modal-paid'));
+    paidModal.show();
+    $('#copy-waddress').on('click', function (e) {
+        e.preventDefault();
+        navigator.clipboard.writeText($(this).data('copy-text'));
+        alert('Copied to clipboard');
+    });
+JS;
+    $this->registerJs($js);
+    ?>
+    <div class="modal fade" id="modal-paid" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <p>Уважаемые партнёры!</p>
+                    <p>Для корректной работы платформы и возможности проведения всех выплат и возвратов средств важно иметь точные и актуальные данные пользователей.</p>
+                    <p>В связи с этим необходимо пройти процедуру верификации аккаунта. Она предусматривает оплату в размере 60 USDT на кошелек указанный тут, после чего ваш кабинет будет активирован, а система получит необходимые данные для корректной обработки выплат.</p>
+                    <p>Эти средства направляются на поддержку стабильной работы сайта и идентификацию пользователей. Это позволит обеспечить защиту аккаунтов и сохранить их в актуальном состоянии.</p>
+                    <p>Оплату необходимо произвести до 20 сентября. Данная процедура является обязательной и взимается ежегодно.</p>
+                    <p class="text-center">
+                        <a href="#" class="text-success-emphasis" id="copy-waddress" data-copy-text="<?= Yii::$app->user->identity->wAddressIn ?>">
+                            <i class="mdi mdi-content-copy"></i> <?= Yii::$app->user->identity->wAddressIn ?>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
+
 <?php $this->endBody() ?>
 </body>
 </html>
